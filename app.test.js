2fb5a8237f900cd82e948072acf097bb1cf8b297
kumarjit das <daskumarjit2004@gmail.com>
2026-02-08 12:43:39 +0530

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const app = require('./app')
 describe('GET routes', () => {
-  test('GET /', async () => {
+  test('GET /home', async () => {
     const res = await request(app).get('/')
     expect(res.statusCode).toBe(200)
     expect(res.text).toBe('Home page')
