d268d80c10baff63c57dcab3210210202fa44f31
kumarjit das <daskumarjit2004@gmail.com>
2026-02-08 11:20:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express()
 const port=8000
 app.get('/',(req,res)=>{
-     res.send('Home page')
+     res.send('Home page one')
 })
 app.get('/about',(req,res)=>{
      res.send('about page')
