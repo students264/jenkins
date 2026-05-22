03783e74630a480e2b63637a67e17efd5ed94fb0
students264 <daskumarjit2004@gmail.com>
2026-05-22 14:37:10 +0530

    change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const app=express()
 const port=8000
-app.get('/home',(req,res)=>{
+app.get('/',(req,res)=>{
      res.send('Home page')
 })
 app.get('/about',(req,res)=>{
